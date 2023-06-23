9345bdd34d81df671109a01dc3ee7ef41937672b
bcmendis <benclintonmendis@gmail.com>
2023-06-23 15:11:30 -0400

    Updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'CTH Assesment',
+  title: 'Login',
   description: 'An attractive, reactive and responsive login page',
 }
 
